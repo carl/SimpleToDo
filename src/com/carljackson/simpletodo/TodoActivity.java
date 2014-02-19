80d84981137fd38e0bf10e41993edce217cb5725
carl <github@carljackson.com>
2014-02-18 23:01:26 -0800

    basic list of items

--- src/com/carljackson/simpletodo/TodoActivity.java
+++ src/com/carljackson/simpletodo/TodoActivity.java
@@ -1,15 +1,28 @@
 package com.carljackson.simpletodo;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class TodoActivity extends Activity {
+	ArrayList<String> items;
+	ArrayAdapter<String> itemsAdapter;
+	ListView listViewItems;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_todo);
+		listViewItems = (ListView) findViewById(R.id.listViewItems);
+		items = new ArrayList<String>();
+		itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+		listViewItems.setAdapter(itemsAdapter);
+		items.add("First Item");
+		items.add("Second Item");
 	}
 
 	@Override
